d2a7a9b3d0d0edc969bdcefc5e42195712fa5ebe
muditjain788 <muditjain788@gmail.com>
2020-03-28 23:17:25 +0530

    more bitwise magic problems

---
+++ BrianKaringhtonAlgorithm.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{	
+	//PROBLEM IS TO COUNT NO OF BITS IN AN INTEGER
+	//It seems pretty easy but logic is very good think first and then see my solution
+	//It solves in O(no_of_set_bits) instead of O(no_of_bits)
+	int n;
+	cout<<"enter the no "<<endl;
+	cin>>n;
+	int res=0;
+	while(n>0)
+	{
+		n=n&(n-1); //what we are doing is removing a bit one by one from right and counting after removing
+			   // CALLED AS BRIAN KARINGHTON ALGORITHM
+		res++;
+	}
+	cout<<"No of SET bits are "<<res<<endl;
+	return 0;
+}
