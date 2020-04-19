e0e1fc36b65968434bb534ea2c79c201f8dad6cc
muditjain788 <muditjain788@gmail.com>
2020-04-20 00:59:42 +0530

    binary search ques

--- findOccurenceInSortedArray.cpp
+++ findOccurenceInSortedArray.cpp
@@ -12,14 +12,25 @@ int leftMost(int arr[],int l,int h,int x)
 	else
 		return leftMost(arr,mid+1,h,x);
 }
-
+int rightMost(int arr[],int l,int h,int x,int n)
+{
+	if(l>h)
+		return -1;
+	int mid=l+(h-l)/2;
+	if(arr[mid]==x&&(mid==n-1||arr[mid+1]!=x))
+		return mid;
+	if(arr[mid]>x)
+		return rightMost(arr,l,mid-1,x,n);
+	else
+		return rightMost(arr,mid+1,h,x,n);
+}
 int main()
 {
 	int T;
 	cin>>T;
 	while(T--)
 	{
-		int n,i;
+		int n,i,j;
 		cin>>n;
 		int arr[n];
 		for(i=0;i<n;i++)
@@ -29,7 +40,11 @@ int main()
 		int x;
 		cin>>x;
 		i=leftMost(arr,0,n-1,x);
-		cout<<i<<endl;
+		j=rightMost(arr,0,n-1,x,n);
+		if(i!=-1&&j!=-1)
+			cout<<j-i+1<<endl;
+		else
+			cout<<-1<<endl;
 	}
 	return 0;
 }
